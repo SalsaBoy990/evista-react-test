cb94f8eaef1b85c0925dce177184bd1945fd50f1
Gulácsi András <guland@protonmail.com>
2021-08-16 02:36:41 +0200

    new: Implement design from UI design scheme

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,39 @@
 import React from "react";
+
 import "./Header.css";
 import evistaLogo from "./evista.png";
 
 class Header extends React.Component {
   render() {
+    const numberOfPairs = this.props.numberOfPairs;
+
     return (
       <header className="header">
-        <img src={evistaLogo} alt="Evista logo" />
+        <img className="header-logo" src={evistaLogo} alt="Evista logo" />
+
+        {this.props.page === "app" && (
+          <div className="header-controls">
+            <span>Deck size: </span>
+
+            <select
+              className="header-deck-size-select"
+              name="numberOfPairs"
+              id=""
+            >
+              {numberOfPairs.map((number) => {
+                return (
+                  <option key={"pairs-" + number} value={number}>
+                    {number}
+                  </option>
+                );
+              })}
+            </select>
+
+            <button className="pure-button header-pure-button-red">
+              Start New Game
+            </button>
+          </div>
+        )}
       </header>
     );
   }
